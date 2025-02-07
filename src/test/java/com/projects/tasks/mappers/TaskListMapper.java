5d70b5dab84d33d3a099015534420f8711496826
Abhijit <abhijitskushwah@gmail.com>
2025-02-07 12:26:50 +0530

    Added TaskList mappers

---
+++ src/test/java/com/projects/tasks/mappers/TaskListMapper.java
@@ -0,0 +1,11 @@
+package com.projects.tasks.mappers;
+
+import com.projects.tasks.domain.dto.TaskListDto;
+import com.projects.tasks.domain.entities.TaskList;
+
+public interface TaskListMapper {
+
+    TaskList fromDto(TaskListDto taskListDto);
+
+    TaskListDto
+}
---
+++ src/test/java/com/projects/tasks/mappers/impl/TaskListMapperImpl.java
@@ -0,0 +1,68 @@
+package com.projects.tasks.mappers.impl;
+
+import com.projects.tasks.domain.dto.TaskDto;
+import com.projects.tasks.domain.dto.TaskListDto;
+import com.projects.tasks.domain.entities.Task;
+import com.projects.tasks.domain.entities.TaskList;
+import com.projects.tasks.domain.entities.TaskStatus;
+import com.projects.tasks.mappers.TaskListMapper;
+import com.projects.tasks.mappers.TaskMapper;
+import org.h2.command.ddl.CreateTable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class TaskListMapperImpl implements TaskListMapper {
+
+    private final TaskMapper taskMapper;
+
+    public TaskListMapperImpl(TaskMapper taskMapper) {
+        this.taskMapper = taskMapper;
+    }
+
+
+    @Override
+    public TaskList fromDto(TaskListDto taskListDto) {
+        return new TaskList(
+                taskListDto.id(),
+                taskListDto.title(),
+                taskListDto.description(),
+                Optional.ofNullable(taskListDto.tasks())
+                        .map(tasks -> tasks.stream().map(taskMapper::fromDto)
+                                .toList()
+                        ).orElse(null),
+                null,
+                null
+        );
+    }
+
+//    @Override
+    public TaskListDto toDto(TaskList taskList){
+        return new TaskListDto(
+                taskList.getId(),
+                taskList.getTitle(),
+                taskList.getDescription(),
+                Optional.ofNullable(taskList.getTasks())
+                        .map(List::size)
+                        .orElse(0),
+                calculateTaskListProgress(taskList.getTasks()),
+                Optional.ofNullable(taskList.getTasks())
+                        .map(tasks -> tasks.stream().map(taskMapper::toDto).toList()
+                        ).orElse(null)
+        );
+    }
+
+    private Double calculateTaskListProgress(List<Task> tasks){
+        if(null == tasks){
+            return null;
+        }
+
+        long closedTaskCount = tasks.stream().filter(task ->
+                TaskStatus.CLOSED == task.getStatus()).count();
+
+        return (double) closedTaskCount / tasks.size();
+    }
+
+}
